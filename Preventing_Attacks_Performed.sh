71b80309092c2f9fd63d7c7e12a71fdd230f6348
Yaswitha Sai Atluri <73469122+YASWITHA27@users.noreply.github.com>
2021-07-19 02:30:22 +0530

    Create Preventing_Attacks_Performed.sh
    After writing and checking the rules written , SNORT is now used in Inline mode where it will be able to detect and as well as prevent the attacks performed by the Kali Linux (Attacker)  on Ubuntu 20.04 9 ( Victim)

---
+++ Preventing_Attacks_Performed.sh
@@ -0,0 +1,18 @@
+Intrusion Prevention:
+
+# Type the following commands in the Root folder of kali Linux (Attacker Machine) and perform the following attacks on victim machine (Ubuntu 20.04)  
+ifconfig
+nmap ip_address of victim
+ping  ip_address of victim
+ftp ip_address of victim
+ssh ip_address of victim
+
+
+
+#Type the following commands in Ubuntu 20.04 to detect the attacks performed by the attacker machine (Kali Linux)
+
+# Commands that has to be followed in victim machine in order to check the rules written and for preventing the attacks 
+
+cd rules
+snort –T –c /etc/snort/snort. conf
+snort –A console  –c /etc/snort/snort. conf
